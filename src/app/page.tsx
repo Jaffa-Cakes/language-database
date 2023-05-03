ef9c9440e38a297fce309d167e6eb706b73645e3
liamparisi <129602696+liamparisi@users.noreply.github.com>
2023-05-03 12:42:41 +1000

    Added Basic Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,91 +4,29 @@ import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-
-  let wimg = 100
+export default function Home(src: string) {
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing LESSGO&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={wimg}
-              height={24}
-              priority
-            />
-          </a>
+    <body className="bg-gray-200 min-h-screen">
+        <div className="container mx-auto flex justify-center items-center h-screen">
+            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+                <h1 className="text-2xl font-bold mb-6">Login</h1>
+                <form action="" method="POST">
+                    <div className="mb-4">
+                        <label className="block text-sm mb-2">Email Address</label>
+                        <input type="email" name="email" id="email" placeholder="Email Address" required className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-sm mb-2">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password" required className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500" />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button type="submit" className="bg-indigo-500 text-white font-bold px-4 py-2 rounded-lg hover:bg-indigo-600">Login</button>
+                        <a href="#" className="text-sm text-indigo-500 hover:text-indigo-600">Forgot password?</a>
+                    </div>
+                </form>
+            </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </body>
   )
 }
