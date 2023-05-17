1602b58cede623e83e02f60c44459a26521180d3
liamparisi <129602696+liamparisi@users.noreply.github.com>
2023-05-17 14:25:30 +1000

    Update advanced-search.ts

--- src/api/advanced-search.ts
+++ src/api/advanced-search.ts
@@ -1,31 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient, Word } from '@prisma/client';
+import { PrismaClient, Language } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   const {
-    source,
-    WordID,
-    EnglishTranslation,
-    WoiWurrung,
-    WWfuzzy,
-    SoneticTranslation,
-    EntryType,
+    id,
+    original_text,
+    translation,
+    fuzzy,
+    sonetics,
+    entry_type,
   } = req.query;
 
   // Build the query object
-  const query: Partial<Word> = {};
-  if (source) query.source = source as string;
-  if (WordID) query.WordID = Number(WordID as string);
-  if (EnglishTranslation) query.EnglishTranslation = EnglishTranslation as string;
-  if (WoiWurrung) query.WoiWurrung = WoiWurrung as string;
-  if (WWfuzzy) query.WWfuzzy = WWfuzzy as string;
-  if (SoneticTranslation) query.SoneticTranslation = SoneticTranslation as string;
-  if (EntryType) query.EntryType = EntryType as string;
+  const query: Partial<Language> = {};
+  if (id) query.id = Number(id as string);
+  if (original_text) query.original_text = original_text as string;
+  if (translation) query.translation = translation as string;
+  if (fuzzy) query.fuzzy = fuzzy as string;
+  if (sonetics) query.sonetics = sonetics as string;
+  if (entry_type) query.entry_type = entry_type as string;
 
   try {
-    const results = await prisma.word.findMany({
+    const results = await prisma.language.findMany({
       where: query,
     });
     res.json(results);
