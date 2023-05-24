c6d75b277dc5619f6a580e559a271293a31d174b
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-05-24 14:04:12 +1000

    Basic Output Page
    Co-Authored-By: T1TRTA <98798836+T1TRTA@users.noreply.github.com>

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -5,7 +5,7 @@ export default function Search() {
   const [inputValue, setInputValue] = useState<string>('');
   const [results, setResults] = useState<string[]>([]); // replace any with the actual data type you're expecting
 
-  const apiUrl = 'http://localhost:3000/api/advanced-search';
+  const apiUrl = 'http://localhost:3000/api/advanced-search?source_id=Thomas12';
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -14,8 +14,9 @@ export default function Search() {
   const handleGet = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     try {
-      const response = await axios.get(`${apiUrl}/${inputValue}`);
-      setResults(prevResults => [...prevResults, response.data]);
+      const response = await axios.get(`${apiUrl}`);
+      setResults(prevResults => response.data);
+      console.log(results)
     } catch (error) {
       console.error(error);
     }
@@ -27,9 +28,18 @@ export default function Search() {
         <input type="text" onChange={handleInputChange} />
         <button onClick={handleGet}>Get</button>
       </form>
-      <ul>
-        {results.map(result => <li>{result.id}</li>)}
-      </ul>
+      <table>
+        {results.map((result) =>
+          <tr>
+            <td><input type='checkbox' /></td>
+            <td className='px-4 py-1'>{result.id}</td>
+            <td className='px-4 py-1 bg-slate-800'>{result.source_id}</td>
+            <td className='px-4 py-1'>{result.temp_id}</td>
+            <td className='px-4 py-1 bg-slate-800'>{result.gloss_translation_in_original}</td>
+            <td className='px-4 py-1'>{result.language_from_original}</td>
+          </tr>
+          )}
+      </table>
     </div>
   );
 }
