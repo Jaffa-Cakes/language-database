a484d10c878a68d67bec17dcad0a6dc84d7a5da9
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-05-31 07:44:21 +1000

    Working

--- src/pages/advanced-search.tsx
+++ src/pages/advanced-search.tsx
@@ -4,123 +4,82 @@ import { useState } from 'react';
 import Menu from './../components/menu';
 import SourceSelect from './../components/dropdown';
 import SearchButton from './../components/searchbutton';
+import Search from '../components/search'
 
 export default function AdvancedSearch() {
   return (
     <div>
       <Menu />
-      <div className="bg-zinc-900 h-screen px-24">
+      <div className="bg-zinc-900 min-h-screen px-24">
         <div className="bg-zinc-900 py-10 flex justify-center">
           <h1 className="text-red-700 font-bold text-3xl font-sans">
             ADVANCED SEARCH
           </h1>
         </div>
-        <form method="GET" action="/api/advancedsearch">
-        <div className="bg-zinc-900 py-8 pb-0 flex justify-start">
-            <label className="px-2 w-full inline-block">
-              <p className="text-white font-bold text-sm font-sans inline-block mr-2">
-                Source ID:
-              </p>
-              <input
-                type="search"
-                id="adv-search-3"
-                name="adv-search-3"
-                placeholder="Any word/term..."
-                className="w-full h-12 px-2"
-              />
-            </label>
+        <form method="GET">
+
+          <div className="py-2 px-2 flex justify-evenly">
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">ID:</label>
+              <input type="text" name="id" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">Source ID:</label>
+              <input type="text" name="source_id" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">Temp ID:</label>
+              <input type="text" name="temp_id" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">Entry ID:</label>
+              <input type="text" name="entry_id" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">Type:</label>
+              <input type="text" name="type" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
           </div>
-          <div className="bg-zinc-900 py-8 pb-0 w-auto flex justify-start">
-            <label className="px-2 w-full inline-block">
-              <p className="text-white font-bold text-sm font-sans inline-block mr-2">
-                Language Form - English:
-              </p>
-              <input
-                type="search"
-                id="adv-search"
-                name="adv-search"
-                placeholder="Any word/term..."
-                className="w-full h-12 px-2"
-              />
-            </label>
+
+          <div className="py-2 px-2 flex justify-evenly">
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">Gloss Translation In Original:</label>
+              <input type="text" name="gloss_translation_in_original" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">Language From Original:</label>
+              <input type="text" name="language_from_original" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">Fuzzy Forms:</label>
+              <input type="text" name="fuzzy_forms" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
+            <div className="flex place-items-center flex-col">
+              <label className="text-lg font-medium mb-1">Sonetic Transliteration:</label>
+              <input type="text" name="sonetic_transliteration" className="w-64 p-1 ml-2 rounded" />
+            </div>
+
+            <div className="w-64 ml-2"></div>
           </div>
-          <div className="bg-zinc-900 py-8 pb-0 flex justify-start">
-            <label className="px-2 w-full inline-block">
-              <p className="text-white font-bold text-sm font-sans inline-block mr-2">
-                Language Form - Woi Wurrung:
-              </p>
-              <input
-                type="search"
-                id="adv-search-2"
-                name="adv-search-2"
-                placeholder="Any word/term..."
-                className="w-full h-12 px-2"
-              />
-            </label>
-          </div>
-          <div className="bg-zinc-900 py-8 pb-0 flex justify-start">
-            <label className="px-2 w-full inline-block">
-              <p className="text-white font-bold text-sm font-sans inline-block mr-2">
-                Language Form - Fuzzy Forms:
-              </p>
-              <input
-                type="search"
-                id="adv-search-2"
-                name="adv-search-2"
-                placeholder="Any word/term..."
-                className="w-full h-12 px-2"
-              />
-            </label>
-          </div>
-          <div className="bg-zinc-900 py-8 pb-0 flex justify-start">
-            <label className="px-2 w-full inline-block">
-              <p className="text-white font-bold text-sm font-sans inline-block mr-2">
-                Excluding these English words:
-              </p>
-              <input
-                type="search"
-                id="adv-search-3"
-                name="adv-search-3"
-                placeholder="Any word/term..."
-                className="w-full h-12 px-2"
-              />
-            </label>
-          </div>
-          <div className="bg-zinc-900 py-8 pb-0 flex justify-start">
-            <label className="px-2 w-full inline-block">
-              <p className="text-white font-bold text-sm font-sans inline-block mr-2">
-                Sonetic Transliterian:
-              </p>
-              <input
-                type="search"
-                id="adv-search-3"
-                name="adv-search-3"
-                placeholder="Any word/term..."
-                className="w-full h-12 px-2"
-              />
-            </label>
-          </div>
-          <div className="bg-zinc-900 py-8 pb-0 flex justify-start">
-            <label className="px-2 w-full inline-block">
-              <p className="text-white font-bold text-sm font-sans inline-block mr-2">
-                Entry Type:
-              </p>
-              <input
-                type="search"
-                id="adv-search-3"
-                name="adv-search-3"
-                placeholder="Any word/term..."
-                className="w-full h-12 px-2"
-              />
-            </label>
-          </div>
+
           <div className="bg-zinc-900 py-8 pb-0 flex justify-center">
             <SearchButton />
           </div>
-          <div className="bg-zinc-900 py-5 w-screen"></div>
-          <div></div>
         </form>
+
+        <hr className="my-4" />
+
+        <Search />
       </div>
+
       <div className="bg-zinc-900 h-96 px-24 ml-48"></div>
       <footer className="bg-red-900 flex py-10 justify-center">
         <p className="font-bold font-sans text-md">
