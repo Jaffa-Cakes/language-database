1129fbe5544ce6f520a58d5f3a9866f2cfa47903
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-07-19 06:03:39 +1000

    importer

--- src/app/import/page.tsx
+++ src/app/import/page.tsx
@@ -1,7 +1,27 @@
 'use client'
 
+import CsvImport from '@/components/CsvImport';
+import { Button } from '@chakra-ui/react';
+import runImport, { CsvData, CsvLexicon, CsvSource } from '@/actions/runImport';
+
+import {useState} from 'react'
+
 export default function Page() {
+    const [sources, setSources] = useState<CsvSource[]>([])
+    const [lexicon, setLexicon] = useState<CsvLexicon[]>([])
+    const [data, setData] = useState<CsvData[]>([])
+
+    async function onImport() {
+        await runImport(sources, lexicon, data);
+    }
+
     return (
-        <h1>Hello, world!</h1>
+        <>
+            <CsvImport name="Sources" setData={setSources}/>
+            <CsvImport name="Lexicon" setData={setLexicon}/>
+            <CsvImport name="Data" setData={setData}/>
+
+            <Button onClick={onImport}>Import</Button>
+        </>
     )
 }
---
+++ src/components/CsvImport.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { Box, Flex, Text } from '@chakra-ui/react';
+import Papa from 'papaparse';
+import { useState } from 'react';
+
+interface Props<T> {
+    setData: (data: T) => void
+    name: string
+}
+
+export default function Component<T>(props: Props<T>) {
+    const {setData, name} = props;
+
+    const [lines, setLines] = useState<number>(0);
+
+    function handleFileSelect(event: React.ChangeEvent<HTMLInputElement>) {
+        const file = event.target.files?.[0];
+
+        Papa.parse(file, {
+            header: true,
+            dynamicTyping: false,
+            complete: function(results: { data: T; }) {
+              setData(results.data);
+              setLines(results.data.length);
+            }
+        });
+    }
+
+    return (
+        <Box mb={5}>
+            <Text mb={2}>{name}:</Text>
+
+            <Flex>
+                <input type="file" accept=".csv" onChange={handleFileSelect}/>
+                <Box background="red.900" rounded="full" px={2} py={1}>
+                    {lines} Lines
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
