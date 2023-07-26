4b6c1b2f1ee54d125e6216ab1d8bb695a02590aa
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-07-27 05:46:41 +1000

    title update

--- src/components/Exporter.tsx
+++ src/components/Exporter.tsx
@@ -12,7 +12,7 @@ export default function Component() {
 
             <Flex w="100vw" position="fixed" bottom={0} direction={expanded ? "column" : "column-reverse"}>
                 <Box pl={3} pr={4} pt={3} pb={2} roundedTopRight="2xl" background="gray.700" w="fit-content" onClick={(e) => setExpanded(!expanded)} cursor="pointer">
-                    <Text>LIFT Exporter</Text>
+                    <Text>Lexicon</Text>
                 </Box>
 
                 <Box h="23vh" flexGrow={1} bottom={0} background="gray.900" p={3} display={expanded ? "block" : "none"}>
