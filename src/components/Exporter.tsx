81a189bc70ce557d7e107c12d9b62e447ef15432
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-07-27 04:25:37 +1000

    clickable rows

--- src/components/Exporter.tsx
+++ src/components/Exporter.tsx
@@ -10,12 +10,12 @@ export default function Component() {
         <>
             <Box h={expanded ? "25vh" : "44px"} mt={3}/>
 
-            <Flex h="25vh" w="100vw" position="fixed" bottom={0} direction={expanded ? "column" : "column-reverse"}>
+            <Flex w="100vw" position="fixed" bottom={0} direction={expanded ? "column" : "column-reverse"}>
                 <Box pl={3} pr={4} pt={3} pb={2} roundedTopRight="2xl" background="gray.700" w="fit-content" onClick={(e) => setExpanded(!expanded)} cursor="pointer">
                     <Text>LIFT Exporter</Text>
                 </Box>
 
-                <Box flexGrow={1} bottom={0} background="gray.900" p={3} display={expanded ? "block" : "none"}>
+                <Box h="23vh" flexGrow={1} bottom={0} background="gray.900" p={3} display={expanded ? "block" : "none"}>
                     
                 </Box>
             </Flex>
--- src/components/SimpleTable.tsx
+++ src/components/SimpleTable.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Table, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr, Text } from "@chakra-ui/react"
+import { useState } from "react";
 
 export interface Props {
     headings: string[];
@@ -9,6 +10,7 @@ export interface Props {
 
 export default function Component(props: Props) {
     let { headings, data } = props;
+    let [toggled, setToggled] = useState<string[]>([]);
 
     const hideScrollbar = {
         '&::-webkit-scrollbar': {
@@ -16,19 +18,32 @@ export default function Component(props: Props) {
         }
     };
 
+    async function toggle(id: string) {
+        if (toggled.includes(id)) {
+            setToggled(toggled.filter((item) => item !== id));
+        } else {
+            setToggled([...toggled, id]);
+        }
+    }
+
     let headingElements = headings.map((heading) => (
         <Th key={heading}>{heading}</Th>
     ));
 
-    let dataElements = data.map((row) => (
-        <Tr key={row[0]}>
-            {
-                row.map((cell) => (
-                    <Td key={cell} maxW={40} overflowX="auto" css={hideScrollbar}>{cell}</Td>
-                ))
-            }
-        </Tr>
-    ));
+    let dataElements = data.map((row) => {
+
+        let color = toggled.includes(row[0]) ? 'green.800' : '';
+
+        return (
+            <Tr key={row[0]} onClick={(e) => toggle(row[0])} backgroundColor={color} cursor="pointer">
+                {
+                    row.map((cell) => (
+                        <Td key={cell} maxW={40} overflowX="auto" css={hideScrollbar}>{cell}</Td>
+                    ))
+                }
+            </Tr>
+        );
+    });
 
     return (
         <>
